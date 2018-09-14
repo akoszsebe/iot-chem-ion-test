85cc3b5861d42dd6676dbbaa94e7c5fcd16aa18d
Zsebe Akos <zsebea@yahoo.com>
2018-09-14 06:52:59 +0000

    lets check real device

--- backend/pi/connection-checker.js
+++ backend/pi/connection-checker.js
@@ -14,23 +14,23 @@ ConnectionChecker.prototype.checking = function () {
   const self = this;
   connectivity(function (online) {
     if (online) {
-      console.log('connected to the internet!', self.alive_number);
+      //console.log('connected to the internet!', self.alive_number);
       self.alive_number = 0;
     } else {
       console.error('sorry, not connected!', self.alive_number);
       if (self.alive_number++ >= 10) {
-        self.heatsourcedevice.turnOffHeatRelay();
-        self.messagequeue.sendMsgToWebServer('Heater:OFF');
-        console.log("---------------------------------Heater off ");
-        self.pumpdevice.turnOffPump();
-        self.messagequeue.sendMsgToWebServer('Pump:OFF');
-        console.log("---------------------------------pump off");
-        setTimeout(function () {
-          if (!self.pumpdevice.pumpWorking && !self.heatsourcedevice.heatSourceWorking) {
-            console.log("---------------------------------Turn of rpy");
-            process.exit(0);
-          }
-        }, 1000);
+        // self.heatsourcedevice.turnOffHeatRelay();
+        // self.messagequeue.sendMsgToWebServer('Heater:OFF');
+        // console.log("---------------------------------Heater off ");
+        // self.pumpdevice.turnOffPump();
+        // self.messagequeue.sendMsgToWebServer('Pump:OFF');
+        // console.log("---------------------------------pump off");
+        // setTimeout(function () {
+        //   if (!self.pumpdevice.pumpWorking && !self.heatsourcedevice.heatSourceWorking) {
+        //     console.log("---------------------------------Turn of rpy");
+        //     process.exit(0);
+        //   }
+        //}, 1000);
       }
     }
   })
