39c024d018b6afd9ad903026c4474b5baceaf3b4
Zsebe Akos <zsebea@yahoo.com>
2018-09-19 09:18:09 +0000

    changed db url

--- config.js
+++ config.js
@@ -6,6 +6,7 @@ config.addons = {};
 config.heroku.domain = 'https://iot-chem-ion-test.herokuapp.com';
 
 config.addons.ampq = 'amqp://iuprolzi:_sJn3dHQffXU02_D9G07W9Ufb9u9kMlM@impala.rmq.cloudamqp.com/iuprolzi';
-config.addons.mongodb = 'mongodb://heroku_ws30tb95:ioaobm86dgeg6ldt5cruq0ch3@ds111876.mlab.com:11876/heroku_ws30tb95';
+config.addons.mongodb = 'mongodb://heroku_rsltzlsr:e5v7hjdk9mlgv6pcccjst0shbk@ds161092.mlab.com:61092/heroku_rsltzlsr';   
+                      //'mongodb://heroku_ws30tb95:ioaobm86dgeg6ldt5cruq0ch3@ds111876.mlab.com:11876/heroku_ws30tb95';
 
 module.exports = config;
